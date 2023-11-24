96bc41b8371270d07c0947aa16d174844b57dadb
J_ManG <kjamong2020@gmail.com>
2023-11-24 18:33:42 +0000

    add commit

--- threads/thread.c
+++ threads/thread.c
@@ -253,6 +253,9 @@ thread_unblock (struct thread *t) {
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
 	list_push_back (&ready_list, &t->elem);
+	/////////////////////////////////////////////
+	list_pop_front(&blocked_list); //block list의 head를 pop;
+	/////////////////////////////////////////////
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
